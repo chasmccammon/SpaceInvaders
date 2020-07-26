867c58939d319721b98190f981734e580e19e5da
Chaz <chasmccammon@gmail.com>
2020-07-27 11:43:08 +1200

    Initial Upload

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Player player = new Player(500, 800);
+            PictureBox box = new PictureBox();
+            box = player.spawn();
+            this.Controls.Add(box); 
+            InitializeComponent();
+            box.ClientSizeChanged(300,300);
+            
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    class Player
+    {
+        private int X;
+        private int Y;
+        public Player(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public PictureBox spawn()
+        {
+            PictureBox ship = new PictureBox();
+            ship.Image = global::SpaceInvaders.Properties.Resources.pngkit_space_ship_png_328881;
+            ship.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            ship.BackColor = System.Drawing.Color.Transparent;
+            ship.Location = new System.Drawing.Point(X, Y);
+            ship.Name = "player";
+            ship.Size = new System.Drawing.Size(150, 150);
+            ship.TabIndex = 0;
+            ship.TabStop = false;
+            return ship;
+        }
+
+        public void moveleft()
+        {
+
+        }
+
+        public void moveright()
+        {
+
+        }
+
+        public void fire()
+        {
+
+        }
+
+        public void die()
+        {
+
+        }
+    }
+}
